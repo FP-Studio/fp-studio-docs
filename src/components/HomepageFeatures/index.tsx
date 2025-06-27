4a7a75163edf068a1c9a03fafbd17954b767d9ec
ayan4m1 <andrew@bulletlogic.com>
2025-06-27 06:50:48 -0400

    fix: apply eslint related cleanup

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import clsx from 'clsx';
+import { clsx } from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import { faCubes } from '@fortawesome/free-solid-svg-icons';

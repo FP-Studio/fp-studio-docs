665320770f2e9111b47fdacdce33854b3b8f61b8
ayan4m1 <andrew@bulletlogic.com>
2025-06-27 06:50:48 -0400

    fix: apply eslint related cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import clsx from 'clsx';
+import { clsx } from 'clsx';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -12,24 +12,21 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header
-      className={clsx('hero hero--primary', styles.heroBanner)}
-      style={{ paddingTop: 20 }}
-    >
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">Your AI Video Editor</p>
         <img
-          src={useBaseUrl('/img/astronaut.png')}
           height={300}
+          src={useBaseUrl('/img/astronaut.png')}
           style={{ marginBottom: 10 }}
         />
         <div className={styles.buttons}>
           <Link
-            style={{ marginRight: 8 }}
             className="button button--secondary button--lg"
+            style={{ marginRight: 8 }}
             to="/docs/get_started"
           >
             Get Started
@@ -50,8 +47,8 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title} Documentation`}
       description="Welcome to FP-Studio's Documentation!"
+      title={`${siteConfig.title} Documentation`}
     >
       <HomepageHeader />
       <main>
